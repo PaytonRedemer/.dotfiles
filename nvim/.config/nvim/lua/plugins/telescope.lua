19721c79f537ab67fd1fa2b1570b72c1a901071b
Payton Redemer <paytonredemer@gmail.com>
2023-01-13 14:44:04 -0600

    add telescope file browser

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -14,9 +14,24 @@ return {
     keys = {
       { "<leader>ff", builtin("find_files"), desc = "Files" },
       { "<leader>fg", builtin("live_grep"), desc = "Grep" },
-      { "<leader>fb", builtin("buffers"), desc = "Buffers"},
+      -- { "<leader>fb", builtin("buffers"), desc = "Buffers"},
       { "<leader>fh", builtin("help_tags"), desc = "Help"},
       { "<leader>fp", builtin("git_files"), desc = "Git" },
     },
   },
+
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+
+    config = function()
+      require("telescope").load_extension "file_browser"
+    end,
+
+    keys = {
+      { "<leader>fb", ":Telescope file_browser<CR>", desc = "File browser"}
+    }
+  }
 }

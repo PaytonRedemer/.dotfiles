32b9fe49bc437bcfa38155f0424d6202f37e2437
Payton Redemer <paytonredemer@gmail.com>
2023-03-08 19:48:24 -0600

    update nvim options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,8 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.clipboard = "unnamedplus"
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.wrap = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -27,3 +30,4 @@ vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "80"
 
 vim.opt.listchars = { eol = "↵", trail = "~", tab = ">-", nbsp = "␣" }
+vim.opt.list = true

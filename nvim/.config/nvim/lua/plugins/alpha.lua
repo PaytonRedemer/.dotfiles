7febdee842df3efb7a7cdcf89ef7fd6245be0888
Payton Redemer <paytonredemer@gmail.com>
2023-10-06 00:17:52 -0500

    add new nvim plugins

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -1,6 +1,7 @@
 return {
   "goolord/alpha-nvim",
   dependencies = "nvim-tree/nvim-web-devicons",
+  enabled = false,
   event = "VimEnter",
   config = function()
     require("alpha").setup(require("alpha.themes.startify").config)

6ef81aae982bfe7cd7477d4097f35683cd82b44f
Payton Redemer <paytonredemer@gmail.com>
2023-11-02 14:13:38 -0500

    update scripts

--- bin/.local/bin/monitorSetup.sh
+++ bin/.local/bin/monitorSetup.sh
@@ -8,10 +8,6 @@ feh --bg-fill ~/Pictures/Wallpapers/orion_nebula.jpg --no-fehbg
 if echo "$displays" | grep -q "HDMI-0"; then
     xrandr --output DP-5 --auto --pos -1920x0 --output DP-0 --primary --mode 1920x1080 --rate 144 --pos 0x0 --output HDMI-0 --auto --pos 1920x0
 
-# Sunshine
-elif echo "$displays" | grep -q "HDMI2"; then
-    xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-
 # Laptop
 elif echo "$displays" | grep -q "eDP1"; then
     xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal

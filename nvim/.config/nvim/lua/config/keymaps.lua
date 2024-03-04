13d6ab60d8a0127388a3460f44130b155371d62e
Payton Redemer <paytonredemer@gmail.com>
2024-03-03 20:20:39 -0600

    disable annoying changes I made in the past

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -24,8 +24,8 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- tab navigation
-vim.keymap.set("n", "<Tab>", ":tabnext<cr>")
-vim.keymap.set("n", "<S-Tab>", ":tabprevious<cr>")
+-- vim.keymap.set("n", "<Tab>", ":tabnext<cr>")
+-- vim.keymap.set("n", "<S-Tab>", ":tabprevious<cr>")
 
 -- better macros
 vim.keymap.set("n", "Q", "@q")
